87dec1535e44930517fd1ef0956fb196288ebdd0
Maruf <alenmaruf.am@gmail.com>
2022-02-10 17:53:41 +0600

    Data Validation and Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,40 +1,75 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required : true,
+        trim : true
+    },
+    email:{
+        type : String,
+        required: true,
+        trim : true,
+        lowercase : true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new error('Email is invalid')
+            }
+        }
+    },
+    password:{
+        type: String,
+        required : true,
+        minlength : 7,
+        trim : true,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new error('Password can not contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default : 0,
+        validate (value){
+            if(value<0){
+                throw new error('Age must be in positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Maruf',
-//     age: 'Md'
-// })
+const me = new User({
+    name: '   Arif    ',
+    email : 'nAMe@JKg.com',
+    password :'155465623'
+})
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
 
 const Task = mongoose.model('task',{
     description: {
-        type: String
+        type: String,
+        required : true,
+        trim : true
     },
     completed: {
-        type: Boolean
+       type: Boolean,
+        default : false
         
     }
 
 })
 
 const task = new Task({
-    description: 'Wake up at 10 a.m',
+    description: 'Eat Lunch',
     completed: false
 })
 task.save().then(()=>{
