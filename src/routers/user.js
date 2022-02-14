8619cefb1281e169e5e7ab396cd1df6ca3f276de
Maruf <alenmaruf.am@gmail.com>
2022-02-14 06:19:39 +0600

    Accepting Authentication Tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 
 
 //Create Users
@@ -32,13 +33,8 @@ router.post('/users/login',async(req,res)=>{
 
 //Read Users
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users);
-    } catch (e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth ,async (req, res) => {
+    res.send(req.user)
 })
 
 
