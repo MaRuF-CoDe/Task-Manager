1124c8004a2e981983666a6efba165ac0fe86aad
Maruf <alenmaruf.am@gmail.com>
2022-02-15 12:08:06 +0600

    Handling Express Errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -136,6 +136,8 @@ const upload = multer({
 
 router.post('/users/me/avatar',upload.single('avatar'),(req,res) => {
     res.send()
+},(error,req,res,next)=>{
+    res.status(400).send({error : error.message})
 })
 
 
