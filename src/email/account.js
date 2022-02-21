91c5e9be6893414ecb131ffe1f66ebf073f729b5
Maruf <alenmaruf.am@gmail.com>
2022-02-21 16:37:37 +0600

    Environment Variables

--- src/email/account.js
+++ src/email/account.js
@@ -1,8 +1,6 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = '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'
-
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeMail = (email,name) =>{
     sgMail.send({
@@ -10,15 +8,6 @@ const sendWelcomeMail = (email,name) =>{
         from:'bangabashi@gmail.com',
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
-    })
-}
-
-const sendCancelationEmail = (email, name) => {
-    sgMail.send({
-        to: email,
-        from: 'andrew@mead.io',
-        subject: 'Sorry to see you go!',
-        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
 }
 
